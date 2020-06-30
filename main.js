4e622813e0a344195f12546a47a2832b2a6760a1
hectorbliss <contacto@fixter.org>
2020-06-30 12:34:57 -0500

    goombas added

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let images = {
 }
 let interval // si queremos apagar
 let frames = 0 // siempre queremos contar
+let enemies = []
 
 // Clases
 class GameItem{
@@ -35,7 +36,7 @@ class Mario extends GameItem{
         super(config) // papi dame mi herencia diunavez tengo una idea de negocio muy buena
     }
     moveLeft = () => {
- // tarea mario no debe salirse
+ // tarea mario no debe salirse 
         this.x-=56
     }
 
@@ -45,9 +46,23 @@ class Mario extends GameItem{
     }
 }
 
+class Goomba extends GameItem{
+    constructor(config){
+        super(config)
+        this.vy = config.vy ? config.vy : 1
+    }
+
+    draw = () => { // sobreescrim¡bimos el draw original
+        this.y+=this.vy // <<================================================= WTF tengo 30 min explicandolo!!
+        ctx.drawImage(this.img,this.x,this.y,this.width,this.height) 
+    }
+}
+
 // Instancias
 let backg = new GameItem({ height:canvas.height, width:canvas.width, image:images.bg })
 let mario = new Mario({x:0,y:450,image:images.mario})
+// si tengo 200 goombas ? o llego 10,000 ?? ---> Array!
+// enemies.push(new Goomba({x:100,y:20,image:images.goomba})) // <<--- esto se puede repetir y no necesitamos recordar los nombres
 
 // Main functions
 function start() {
@@ -61,9 +76,13 @@ function update() { // esta mierda se repite infinitamente
     // aquí dibujamo
     // antes borramo
     ctx.clearRect(0,0,canvas.width, canvas.height)
-    // redibujamo cada uno de los elementos del videjuego (instancias)
+    // redibujamo cada uno de los elementos del videjuego (instancias) <-- regla
     backg.draw()
     mario.draw()
+    // si lo quiero en mi videojuego lo tengo que meter a update
+    drawGoombas()
+    // generate stuff:
+    if(frames%10 === 0) generateGoomba() // cual es el tiempo optimo para que no se encimen ...
 }
 
 function stop() {
@@ -72,6 +91,19 @@ function stop() {
 
 // aux functions
 // <3 de tu juego
+function generateGoomba () { 
+    // queremos: enemies.push(new Goomba({x:100,y:20,image:images.goomba}))
+    let x = Math.floor((Math.random() * (canvas.width-5)) + 5)
+    //generate random vy
+    let goomba = new Goomba({x,image:images.goomba, vy:2})
+    enemies.push(goomba)
+}
+
+function drawGoombas () {  // ya podemos dibujarlos a todos
+    enemies.forEach(goomba=>{ // porque es un ciclo 
+        goomba.draw()
+    })
+}
 
 // listeners
 addEventListener('keydown', e=>{
